0bfac84ee1c7d135dad1833194aacf56f52be435
ElodieBerthaud <elodie.berthaud@epitech.eu>
2018-10-04 20:25:05 +0300

    Error management on Login + Forgot password.

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -9,7 +9,7 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import loader from '../images/loaders/bars-loader.gif';
-import check from '../images/loaders/animated-check.gif';
+const emailRegex = require('email-regex');
 
 class Login extends Component{
 
@@ -18,44 +18,80 @@ class Login extends Component{
 
         this.state = {
             open: false,
-            loader: false
+            loader: false,
+            email: '',
+            password: '',
+            emailforgot: '',
+            requiredText1: '',
+            requiredText2: '',
+            requiredText3: ''
         };
+
+        this.handleCheckForm = this.handleCheckForm.bind(this);
+        this.handleClickOpen = this.handleClickOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
+
     }
 
-    handleClickOpen = () => {
+    handleClickOpen(){
         this.setState({ open: true });
     };
 
-    handleClose = () => {
+    handleClose(){
         this.setState({ open: false });
         this.setState({loader: false});
     };
 
-    handleLoader = () => {
-        this.setState({ loader: true });
+    handleCheckForm(){
+        if (!emailRegex({exact: true}).test(this.state.emailforgot)){
+            this.setState({ requiredText3: 'L\'adresse Email n\'est pas valide.'});
+        }
+        else{
+            this.setState({ loader: true });
+        }
     }
 
+    handleChange(evt){
+        this.setState({[evt.target.name]: evt.target.value});
+        this.state.email !== '' ? this.setState({requiredText1 : ''}) : false;
+        this.state.password !== '' ? this.setState({requiredText2 : ''}) : false;
+        this.state.emailforgot !== '' ? this.setState({requiredText3 : ''}) : false;
+        console.log(evt.target.name);
+    }
 
     render() {
         return (
             <div className="WrapLogin">
+                <div style={{margin:'5%'}}>
                 <div className="text-center title">Connexion</div>
                 <form>
                     <TextField
                         style={{width: '90%'}}
                         hintText="Votre identifiant"
                         floatingLabelText="Identifiant"
+                        name="email"
+                        errorText={this.state.requiredText1}
+                        value={this.state.email}
+                        onChange={this.handleChange}
                     /><br/>
                     <TextField
                         style={{width: '90%'}}
                         hintText="Votre mot de passe"
                         floatingLabelText="Mot de passe"
+                        name="password"
+                        type='password'
+                        errorText={this.state.requiredText2}
+                        value={this.state.password}
+                        onChange={this.handleChange}
                     /><br/>
                     <div className="button"><RaisedButton label="Se connecter" primary={true}/></div>
                     <a onClick={this.handleClickOpen} style={{cursor:'pointer'}}>
-                        <div style={{float: 'right', fontSize: '0.8' + 'em'}}> Mot de passe oublié ?</div>
+                        <div style={{float: 'right', fontSize: '0.8' + 'em', margin: '2%'}}> Mot de passe oublié ?</div>
                     </a>
                 </form>
+                </div>
 
                 <Dialog
                     open={this.state.open}
@@ -64,7 +100,7 @@ class Login extends Component{
                 >
                     <div style={this.state.loader ? {textAlign:'center', margin: '10%'} : {display:'none'}}>
                         <img src={loader} style={{display: 'inherit', margin: '0 auto'}} />
-                        Veulliez patienter, votre mail est en cours d'envoie...
+                        Veulliez patienter, votre mail est en cours d'envoi...
                     </div>
                     <div style={this.state.loader ? {display:"none"} : {display:''}}>
                     <DialogTitle id="form-dialog-title">Mot de passe oublié</DialogTitle>
@@ -75,17 +111,21 @@ class Login extends Component{
                         <TextField
                             autoFocus
                             margin="dense"
+                            name="emailforgot"
                             id="name"
                             label="Email Address"
                             type="email"
+                            value={this.state.emailForgot}
                             fullWidth
+                            errorText={this.state.requiredText3}
+                            onChange={this.handleChange}
                         />
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={this.handleClose} color="primary">
                             Annuler
                         </Button>
-                        <Button onClick={this.handleLoader} color="primary">
+                        <Button onClick={this.handleCheckForm} color="primary">
                             Recevoir mon nouveau mot de passe
                         </Button>
                     </DialogActions>
@@ -94,10 +134,6 @@ class Login extends Component{
             </div>
         );
     };
-}
-
-var button = {
-    paddingTop: 10
 }
 
 export default Login;
